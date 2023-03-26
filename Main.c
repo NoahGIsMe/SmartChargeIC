badf411f6dc5232cd9d4b71f2290b859a85b8e51
Alita <justinlu264@gmail.com>
2023-03-26 01:00:52 -0700

    Added EEPROM and comments for future edits

--- Main.c
+++ Main.c
@@ -1,5 +1,6 @@
-#include <RTClib.h>
 #include <Wire.h>
+#include <EEPROM.h>
+#include <RTClib.h>
 #include "LTC2941.h"
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -13,6 +14,17 @@
 int PWMpin = 4;                                                     //Pin 4 used due to higher base PWM frequency of 980Hz
 int tempPin = A1;                                                   //TMP36's Analog Vout (Sense) pin is connected to pin A1 on Arduino
 
+//float coulomb = 0;
+float mAh;
+float percent;
+float prevBatteryTemp;
+//float currentBatteryTemp = 0;
+int dutyCycle;
+int dutyCycleTempLoss;                                              //Stores duty cycle decrease due to temperature limit
+bool fastCharge;
+uint8_t maxChargeLimit;
+bool alarmSet;
+
 RTC_DS1307 rtc;                                                     //Declares RTC object
 DateTime currentTime;
 uint8_t alarmHour;
@@ -20,14 +32,6 @@ uint8_t alarmMinute;
 bool isPM;
 uint8_t alarmHourDiff;
 uint8_t alarmMinuteDiff;
-
-//float coulomb = 0;
-float mAh = 0;
-float percent = 0;
-float prevBatteryTemp = 0;
-//float currentBatteryTemp = 0;
-int dutyCycle = 0;
-int dutyCycleTempLoss = 0;                                          //Stores duty cycle decrease due to temperature limit
 
 //post-testing table of duty cycle values vs. battery percentage/voltage (3-7-4.2V)
 
@@ -54,9 +58,14 @@ void setup(void) {
     //Serial.println("LTC2941 Raw Data");
     ltc2941.initialize();
     ltc2941.setBatteryFullMAh(1000);
+
+    loadScreen();
 }
 
 ISR(TIMER1_COMPA_vect) {                                            //Timer1 compare interrupt service routine
+    percent = ltc2941.getPercent();
+    //call print/updateBatteryPercentage function - only update if it changes
+
     //currentBatteryTemp = getBatteryTemp();                        //Reads current battery temperature
     int tempReading = analogRead(tempPin);                          //Reads in and converts TMP36's Vout to int between 0-1023
     float tempVoltage = tempReading * aref_voltage / 1023.0;        //Converts Vout from int to voltage between 0-3.3V
@@ -76,8 +85,8 @@ ISR(TIMER1_COMPA_vect) {                                            //Timer1 com
 
 void loop(void) {
     //coulomb = ltc2941.getCoulombs();
-    mAh = ltc2941.getmAh();
-    percent = ltc2941.getPercent();
+    //mAh = ltc2941.getmAh();
+    //percent = ltc2941.getPercent();
     // Serial.print(coulomb);
     // Serial.print("C,");
     // Serial.print(mAh);
@@ -85,15 +94,53 @@ void loop(void) {
     // Serial.print(percent);
     // Serial.print("%");
     // Serial.println();
-    delay(1000);
+    // delay(1000);
 
+    //if (setChargeSpeed.pressed)
+        fastCharge ^= 1;
+        EEPROM.update(0, fastCharge);
+    //if (setMaxChargeLimit.pressed)
+        //maxChargeLimit = userValueFromScreen
+        EEPROM.update(1, maxChargeLimit);
     //if (setAlarmButton.pressed)
+        alarmSet ^= 1;
+        EEPROM.update(2, alarmSet);
+        setAlarm();
+}
+
+void loadScreen() {
+    fastCharge = EEPROM.read(0);                                    //Loads charging speed setting from EEPROM
+    maxChargeLimit = EEPROM.read(1);                                //Loads maximum charge limit setting from EEPROM
+    alarmSet = EEPROM.read(2);
+
+    //call print/updateBatteryPercentage function
+    //call print/updateColorOfLightningBolt function if charging
+    //print "Charging Mode", "Slow", and "Fast" in fixed positions
+    //draw rounded rectangle in fixed position (https://learn.adafruit.com/adafruit-gfx-graphics-library/graphics-primitives#rounded-rectangles-2002792)
+    //call drawChargeSpeedCircle (fixed slow/fast positions)
+    //print "Maximum Charge Limit" in fixed position
+    //call print/updateChargeLimit function to print text
+    //draw button that brings up numpad to enter charge limit
+    //print "Alarm" in fixed position
+    //call print/updateAlarmTime function
+    //draw rounded rectangle in fixed position
+    //call setAlarm function to draw circle position
+}
+
+// void showNumpad() {
+
+// }
+
+void setAlarm() {
+    if (alarmSet == 1) {
         currentTime = RTC.now;
         alarmHourDiff = (alarmHour >= currentTime.hour() ? 0 : 24) + alarmHour - currentTime.hour() - (alarmMinute < currentTime.minute() ? 1 : 0);
         alarmMinuteDiff = (alarmMinute >= currentTime.minute() ? 0 : 60) + alarmMinute - currentTime.minute();
+    }
+    //draw toggle button/circle (+ alarmSet * ~50pixelOffset)
 }
 
-// float getBatteryTemp(){
+// float getBatteryTemp() {
 //     int tempReading = analogRead(tempPin);                          //Reads in and converts TMP36's Vout to int between 0-1023
 //     float tempVoltage = tempReading * aref_voltage / 1023.0;        //Converts Vout from int to voltage between 0-3.3V
 //     float tempC = (tempVoltage - 0.5) * 100;                        //Converts voltage to °C with 10mV/°C and 500mV offset
