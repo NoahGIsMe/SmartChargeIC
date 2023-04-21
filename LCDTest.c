f217699a0cc04c71e86401e6a1da06eb39f9a88f
Jesus Cota Felix <64045279+jesuscotafelix@users.noreply.github.com>
2023-04-20 18:50:25 -0700

    Update LCDTest.c

--- LCDTest.c
+++ LCDTest.c
@@ -1,21 +1,29 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include <stdint.h>
+#include "TouchScreen.h"
 
 // For the Adafruit shield, these are the default.
+// #define TFT_MISO 50 //Needs to be digital pin 50
+// #define TFT_CLK 52 // Needs to be digital pin 52
+// #define TFT_RST 49 //Needs to be digital pin 59
+// #define TFT_MOSI 51 //Needs to be digital pin 51
+#define YP A2 //has to be an analog pin
+#define XM A3 //Has to be an analog pin
+
+//Can be any pin as long as its digital
 #define TFT_DC 46
 #define TFT_CS 48
-#define TFT_MISO 50
-#define TFT_CLK 52
-#define TFT_RST 49
-#define TFT_MOSI 51
+#define YM 42  
+#define XP 40
 
 
 
+void Startup();
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// If using the breakout, change pins as desired
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+TouchScreen ts = TouchScreen(XP,YP,XM,YM,300);
 
 //text size 1 = 7 pixels max
 //text size 2 = 14
@@ -49,12 +57,15 @@ void loop() {
   
   //this code is only for testing whether i have to update the entire screen or i can only update a certain portion.
   //probably gonna use a method to create a rectangle over the text so i can refresh the screen with a new value/image
-  tft.setCursor(195,90);
-  tft.fillRect(0, 240, 320, 10, ILI9341_BLACK);
-  tft.fillRect(0, 240, 320, 10, ILI9341_BLUE);
-  tft.fillRect(0, 240, 320, 10, ILI9341_WHITE);
   // ;tft.drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
+Choose_Charge_Speed();
 
+Choose_Max_Capacity();
+
+// if(p.z>ts.pressureThreshhold() && p.y >630 && p.y<730)
+//   if(p.x >430 && p.x < 570){
+
+//   }
 
 }
 
@@ -73,7 +84,7 @@ void Startup(){
   tft.setCursor(0, 20);
   tft.print("SmartChargeIC");
 
-  //prompts user for charge speed preference
+  //prompts user for charge speed preferenc 
   tft.setTextSize(2);
   tft.println("\n\nPlease select charge speed");
   
@@ -88,7 +99,32 @@ void Startup(){
   tft.fillRect(20+100,100,100,30,ILI9341_BLUE);
 
   //Print Maximum charge setting
-  tft.setCursor(0,175);
-  
+  tft.setCursor(40,175);
+  tft.setTextSize(2);
   tft.print("Maximum Charge");
+  tft.drawRect(90, 210, 60, 50, ILI9341_BLACK);
+  tft.setCursor(95, 213);
+  tft.print();
+}
+
+void Choose_Charge_Speed(){
+  TSPoint p = ts.getPoint();
+  if (p.z > ts.pressureThreshhold && p.y >315 && p.y<420) {
+  if(p.x>210 && p.x<430){
+      tft.fillRect(20,100,100,30,ILI9341_DARKGREEN);
+      tft.fillRect(20+100,100,100,30,ILI9341_BLUE);
+      Serial.print("Slow Charging\n");
+      delay(200);
+  }
+  if(p.x>550 && p.x<820){
+    tft.fillRect(20+100,100,100,30,ILI9341_NAVY);
+    tft.fillRect(20,100,100,30,ILI9341_GREEN);
+    Serial.print("Fast Charging\n");
+    delay(200);
+  }
+}
+}
+
+void Choose_Max_Capacity(){
+
 }
