e0e4ad6c9fed3821615eaf8b14bc8f5de09932f2
Alyta99 <81391911+Alyta99@users.noreply.github.com>
2023-04-25 01:27:57 -0700

    Started integrating screen code

--- Main.c
+++ Main.c
@@ -2,6 +2,11 @@
 #include <EEPROM.h>     //EEPROM editor library
 #include <RTClib.h>     //RTC library
 #include "LTC2941.h"    //Coulomb counter library
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <stdint.h>
+#include "TouchScreen.h"
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
     #define Serial SerialUSB
@@ -11,6 +16,13 @@
 
 #define aref_voltage 3.3                                            //ARef pin is tied to 3.3V to decrease noise in TMP36 temperature reading
 #define sensorPin A0
+
+#define YP A2                                                       //Have to be analog pins
+#define XM A3
+#define TFT_DC 46                                                   //Can be any pin as long as its digital
+#define TFT_CS 48
+#define YM 42  
+#define XP 40
 
 int PWMpin = 4;                                                     //Pin 4 used due to higher base PWM frequency of 980Hz
 int tempPin = A1;                                                   //TMP36's Analog Vout (Sense) pin is connected to pin A1 on Arduino
@@ -25,7 +37,7 @@ int dutyCycle;
 int dutyCycleTempLoss;                                              //Stores duty cycle decrease due to temperature limit
 int interruptCounter;
 bool fastCharge;
-uint8_t maxChargeLimit;
+int8_t chargeLimit = 80;                                            //Sets default maximum battery percentage to 80%
 bool alarmSet;
 bool limitButton;
 bool speedButton;
@@ -67,23 +79,50 @@ void setup(void) {
 }
 
 void loop(void) {
-    coulomb = ltc2941.getCoulombs();                                //reads charge in coulombs
-    mAh = ltc2941.getmAh();                                         //reads charge in mAh
-    percent = ltc2941.getPercent();                                 //reads battery percentage
-    setAlarm();                                                     //set the alarm
-    getBatteryTemp(void);                                           //get the battery temperature
-    setChargeSpeed();                                               //set the charge speed
-    setMaxChargeLimit();                                            //set the charge capacity
+    TSPoint p = ts.getPoint();
+    if (p.z > ts.pressureThreshhold) {
+        if (p.y > 295 && p.y < 400) {                               //Charge speed is being modified
+            if (p.x > 210 && p.x < 430 && fastCharge != 0) {
+                fastCharge = 0;
+                EEPROM.update(0, fastCharge);
+                showChargeSpeed();
+            }
+            else if (p.x > 550 && p.x < 820 && fastCharge != 1) {
+                fastCharge = 1;
+                EEPROM.update(0, fastCharge);
+                showChargeSpeed();
+            }
+        }
+        else if (p.y > 580 && p.y < 680) {                          //Maximum charge limit is being modified
+            if (p.x > 230 && p.x < 280 && chargeLimit > 0) {
+                setChargeLimit(-5);
+            }
+            else if (p.x > 745 && p.x < 805 && chargeLimit < 100) {
+                setChargeLimit(5);
+            }
+        }
+        else if () {
+            //alarmSet
+        }
+        else if () {
+            //alarmToggle
+        }
+    }
+    
+    //coulomb = ltc2941.getCoulombs();                                //reads charge in coulombs
+    //mAh = ltc2941.getmAh();                                         //reads charge in mAh
+    //percent = ltc2941.getPercent();                                 //reads battery percentage
+    //setAlarm();                                                     //set the alarm
+    //getBatteryTemp(void);                                           //get the battery temperature
+    //setChargeSpeed();                                               //set the charge speed
+    //setMaxChargeLimit();                                            //set the charge capacity
 }
 
 ISR(TIMER1_COMPA_vect) {                                            //Timer1 compare interrupt service routine
-    if (++interruptCounter %= 10) {
+    if (++interruptCounter %= 5) {
         percent = ltc2941.getPercent();                             //call print/updateBatteryPercentage function - only update if it changes
 
         currentBatteryTemp = getBatteryTemp()                       //Reads current battery temperature
-        int tempReading = analogRead(tempPin)                       //Reads in and converts TMP36's Vout to int between 0-1023
-        float tempVoltage = tempReading * aref_voltage / 1023.0;    //Converts Vout from int to voltage between 0-3.3V
-        float currentBatteryTemp = (tempVoltage - 0.5) * 100;       //Converts voltage to °C with 10mV/°C and 500mV offset
         if (currentBatteryTemp > 45) {
             if (currentBatteryTemp >= prevBatteryTemp)              //If battery temp exceeds 45°C limit and is not decreasing
                 //analogWrite(PWMpin, dutyCycle[getBatteryPercent] - ++dutyCycleTempLoss);   //Decrease PWM duty cycle/charging current and record that duty cycle has been temporarily decreased
@@ -99,9 +138,13 @@ ISR(TIMER1_COMPA_vect) {                                            //Timer1 com
 }
 
 void loadScreen() {
-    fastCharge = EEPROM.read(0);                                    //Loads charging speed setting from EEPROM
-    maxChargeLimit = EEPROM.read(1);                                //Loads maximum charge limit setting from EEPROM
-    alarmSet = EEPROM.read(2);
+    tft.begin();
+    Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+    TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+    
+    //fastCharge = EEPROM.read(0);                                    //Loads charging speed setting from EEPROM
+    //maxChargeLimit = EEPROM.read(1);                                //Loads maximum charge limit setting from EEPROM
+    //alarmSet = EEPROM.read(2);
 
     //call print/updateBatteryPercentage function
     //call print/updateColorOfLightningBolt function if charging
@@ -141,20 +184,53 @@ float getBatteryTemp() {
     return tempC;
 }
 
-void setChargeSpeed() {
-    if (speedButton == true) {
-        fastCharge ^= 1;
-        digitalWrite(blueLED, HIGH);
-        EEPROM.update(0, fastCharge);
+void showChargeSpeed() {
+    if(fastCharge){
+        tft.fillRect(20,90,100,30,ILI9341_DARKGREEN);
+        tft.fillRect(20+100,90,100,30,ILI9341_BLUE);
+        Serial.println("Fast Charging");
     }
     else {
-        digitalWrite(blueLED, LOW);
+        tft.fillRect(20+100,90,100,30,ILI9341_NAVY);                //20+100?
+        tft.fillRect(20,90,100,30,ILI9341_GREEN);
+        Serial.println("Slow Charging");
     }
+    digitalWrite(blueLED, fastCharge);
+    delay(200);                                                     //is this delay necessary? no delay in setchargeLimit
 }
 
-void setMaxChargeLimit() {
-    if (limitButton == true) {
-        maxChargeLimit = userValueFromScreen
-        EEPROM.update(1, maxChargeLimit);
+void setChargeLimit(int8_t percentChange) {
+    if (percentChange > 0) {
+        tft.fillRect(175, 195, 35, 28, ILI9341_LIGHTGREY);
+        tft.setCursor(175, 195);
+        tft.setTextSize(3);
+        tft.print("+5");
+        delay(300);
+        tft.fillRect(175, 195, 35, 28, ILI9341_WHITE);
+        tft.setCursor(175, 195);
+        tft.setTextSize(3);
+        tft.print("+5");
+        tft.fillRect(91, 181, 58, 48, ILI9341_WHITE); //Don't change this (note to self)
     }
+    else if (percentChange < 0) {
+        tft.fillRect(30, 195, 35, 28, ILI9341_LIGHTGREY);
+        tft.setCursor(30, 195);
+        tft.setTextSize(3);
+        tft.print("-5");
+        delay(300);
+        tft.fillRect(30, 195, 35, 28, ILI9341_WHITE);
+        tft.setCursor(30, 195);
+        tft.setTextSize(3);
+        tft.print("-5");
+        tft.fillRect(91, 181, 58, 48, ILI9341_WHITE); //Don't change this (note to self)
+    }
+    chargeLimit += percentChange;
+    EEPROM.update(1, chargeLimit);
+    if (chargeLimit == 100) {
+        tft.setCursor(95, 195);
+    }
+    else {
+        tft.setCursor(105, 195);
+    }
+    tft.print(chargeLimit);
 }
