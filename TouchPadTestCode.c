9f0d92389b4d5103ccba056084ddca9fc11758b2
Jesus Cota Felix <64045279+jesuscotafelix@users.noreply.github.com>
2023-04-26 06:28:18 -0700

    Update TouchPadTestCode.c

--- TouchPadTestCode.c
+++ TouchPadTestCode.c
@@ -1,3 +1,57 @@
+
+void keyPad() {
+  //resets screen to white
+  tft.fillScreen(ILI9341_WHITE);
+  tft.setTextColor(ILI9341_BLACK);
+  //Creates the horizontal lines of the keypad for the alarm
+  tft.drawLine(0, 118, 240, 118, ILI9341_BLACK);
+  tft.drawLine(0, 170, 240, 170, ILI9341_BLACK);
+  tft.drawLine(0, 219, 240, 219, ILI9341_BLACK);
+  tft.drawLine(0, 271, 240, 271, ILI9341_BLACK);
+
+  //Creates the vertical lines for the keypad for the alarm
+  tft.drawLine(80, 118, 80, 320, ILI9341_BLACK);
+  tft.drawLine(160, 118, 160, 320, ILI9341_BLACK);
+
+  //Creates entry box for input
+  tft.drawRect(24, 11, 198, 65, ILI9341_BLACK);
+
+  //Sets the numbers for the keypad
+  tft.setTextSize(3);
+  tft.setCursor(35, 135);
+  tft.print("1");
+
+  tft.setCursor(115, 135);
+  tft.print("2");
+
+  tft.setCursor(195, 135);
+  tft.print("3");
+
+  tft.setCursor(35, 185);
+  tft.print("4");
+
+  tft.setCursor(115, 185);
+  tft.print("5");
+
+  tft.setCursor(195, 185);
+  tft.print("6");
+
+  tft.setCursor(35, 235);
+  tft.print("7");
+
+  tft.setCursor(115, 235);
+  tft.print("8");
+
+  tft.setCursor(195, 235);
+  tft.print("9");
+
+  tft.setCursor(115, 285);
+  tft.print("0");
+  //Sets the text inside the box of the alarm input box
+
+}
+
+
 void Set_Alarm_Hour(){
 
   keyPad();
